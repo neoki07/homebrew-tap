dba93faf7df01489cebd19ffce2a92a6b4461f18
neoki <ot07neoki@gmail.com>
2024-07-03 20:03:55 +0900

    upgrade: update gibra version

--- gibra.rb
+++ gibra.rb
@@ -1,10 +1,10 @@
 class Gibra < Formula
   desc "A command-line tool that enables interactive branch selection for git"
   homepage "https://github.com/neoki07/gibra"
-  url "https://github.com/neoki07/gibra/releases/download/v0.4.1/gibra-0.4.1-x86_64-apple-darwin.tar.gz"
-  sha256 "84be65a341b04a336eb9aa749f287443088f38ef8fb5f030d32393b11c61ba6b"
+  url "https://github.com/neoki07/gibra/releases/download/v0.4.2/gibra-0.4.2-x86_64-apple-darwin.tar.gz"
+  sha256 "4dedd0a06c8bf14503c9f32c3a750d6ab23c31ee042141d54bdea8203d3c3541"
   license "MIT"
-  version "0.4.1"
+  version "0.4.2"
 
   def install
     bin.install "gibra"
